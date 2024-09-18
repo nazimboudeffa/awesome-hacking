3af9b906b04087fd054bde8588c1249f21e04951
Nazim Boudeffa <boudeffa@gmail.com>
2024-09-18 22:17:15 +0200

    Update parrot.sh

--- parrot.sh
+++ parrot.sh
@@ -9,3 +9,6 @@ cd ~/Documents
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
 go install github.com/ffuf/ffuf/v2@latest
+# XSS
+sudo snap install dalfox
+# https://github.com/s0md3v/XSStrike
