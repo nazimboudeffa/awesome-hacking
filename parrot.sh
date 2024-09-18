04aa1e045aef5580c19a53d917d19cfa5d2fa7cd
Nazim Boudeffa <boudeffa@gmail.com>
2024-09-18 18:33:59 +0200

    Update parrot.sh

--- parrot.sh
+++ parrot.sh
@@ -2,4 +2,6 @@ sudo apt install strace
 sudo apt install ltrace
 sudo apt install amass
 gem install wpscan
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
 go install github.com/ffuf/ffuf/v2@latest
