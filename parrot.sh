b40567159294bc6ff6577fa780d620801cc900cc
Nazim Boudeffa <boudeffa@gmail.com>
2024-09-20 12:07:46 +0200

    Update parrot.sh

--- parrot.sh
+++ parrot.sh
@@ -1,17 +1,25 @@
+#########
 # REVERSE
+#########
 sudo apt install strace
 sudo apt install ltrace
+#########
 # WORDPRESS
+#########
 gem install wpscan
 # wpscan wpscan --url https://target.com
+#########
 # RECON
+#########
 sudo apt install amass
 cd ~
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
 go install github.com/ffuf/ffuf/v2@latest
 PATH=$PATH:~/user/go/bin
+#########
 # XSS
+#########
 sudo snap install dalfox
 git clone https://github.com/s0md3v/XSStrike
 cd XSStrike
